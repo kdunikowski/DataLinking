6b85b3137ec27d9f502ff19d0156ca3405c6e347
dunikowskik <dunikowskik@tt.com.pl>
2020-07-24 14:59:33 +0200

    initial commit

---
+++ src/main/java/com/kdunikowski/data/linking/DataLinking.java
@@ -0,0 +1,13 @@
+package com.kdunikowski.data.linking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLinking {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataLinking.class, args);
+	}
+
+}
